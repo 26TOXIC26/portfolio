3deb49df83ba2b19e19c18d29fca13197ca2072b
26TOXIC26 <bdmousaid@gmail.com>
2025-05-02 20:18:16 +0100

    2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="title">26 TOXIC 26</h1>
+    </div>
+  );
+}
+
+export default App;
